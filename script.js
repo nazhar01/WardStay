6060e74bcc83ae03616ae9f5524a991d8266692d
Nurul <nurulafifah_azhar91@rocketmail.com>
2026-08-05 00:33:08 +0800

    Intial WardStay Project

---
+++ script.js
@@ -0,0 +1,26 @@
+/* -------- DOCTOR UPDATE -------- */
+const updateButton = document.getElementById("updateBtn");
+const patientStatus = document.getElementById("patient-status");
+const timeline = document.getElementById("timeline");
+
+let doctorUpdateAdded = false;
+
+updateButton.addEventListener("click", function () {
+
+   if (patientStatus.textContent === "Waiting for Update") {
+
+      patientStatus.textContent = "Doctor has completed today's rounds.";
+
+      if (!doctorUpdateAdded) {
+         timeline.innerHTML +=
+            "<li><strong>11:30 AM:</strong> Doctor completed today's rounds.</li>";
+         doctorUpdateAdded = true;
+      }
+
+   } else {
+
+      patientStatus.textContent = "Waiting for Update";
+
+   }
+
+});
